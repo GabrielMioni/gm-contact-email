6e1d90cab188aef6bdb573ebe5357d92888ed96f
GabrielMioni <gabriel.mioni@gmail.com>
2017-08-12 12:19:48 -0700

    Added improved documenation.

--- js/gm-contact.js
+++ js/gm-contact.js
@@ -1,14 +1,22 @@
 /**
- * This JS file is part of the gm-contact-email plugin.
+ * @package     GM-Contact
+ * @author      Gabriel Mioni <gabriel@gabrielmioni.com>
+ *
+ * This file does the following:
+ * 1. Processes Ajax requests to send email from the GM Contact Form.
+ * 2. Performs JS form validation before allowing a submit.
+ * 3. Displays validation errors and responses to the Contact Form user.
+ *
+ * Ajax handler is at called using the gm_contact_ajax() function on gm-contact-email.php.
  */
 
 (function($) {
 
     /**
-     * If response from gm-contact.php is 1, display thank you message. Else, parse JSON object
+     * If response from gm-contact-email.php is 1, display thank you message. Else, parse JSON object
      * into different error messages and display them.
      *
-     * @param   {int|string}    resp     Response from gm_contact_ajax() in gm-contact.php
+     * @param   {int|string}    resp     Response from gm_contact_ajax() in gm-contact-email.php
      * @param   {HTMLElement}   form     The HTML form element being submitted.
      * @param   {int}           msg_type Set to 1 for success message. Else will display generic error.
      */
@@ -80,13 +88,19 @@
     /**
      * Checks the form inputs for errors.
      *
-     * @param form_data
-     * @returns {boolean}
+     * @param   form_data   {String}    The serialized form data being submitted.
+     * @returns {boolean}   If the form data is valid, return true. Else set error messages.
      */
     function validate_form(form_data) {
 
+        // Use the serialized data string to create an object with properties for each input type.
         var form_input_obj = create_form_input_obj(form_data);
 
+        /*
+         * Check the form_input_obj object's properties to see if any fail to pass validation.
+         * - If everything var error_obj will be true.
+         * - If there are validation errors, error_obj will be a JS object with properties representing errors.
+         */
         var error_obj = create_error_obj(form_input_obj);
 
         if (error_obj === true)
@@ -98,10 +112,9 @@
     }
 
     /**
-     * Creates a JS object from the serialized form input data. Used to easily
-     * evaluate the input values.
+     * Creates a JS object from the serialized form input data. Used to easily evaluate the input values.
      *
-     * @param form_data
+     * @param   form_data   {String}    The serialized form data being submitted
      * @returns {{name: string, email: string, company: string, message: string}}
      */
     function create_form_input_obj(form_data) {
@@ -116,8 +129,11 @@
         for (var i = 0 ; i < form_array.length ; ++i)
         {
             var current_array_elm = form_array[i];
+
+            // Try to split the current array element into a key value pair.
             var split_current = current_array_elm.split('=');
 
+            // If the split worked, set the appropriate form_input_obj object's property to the value of split_current[1].
             if (split_current.length === 2)
             {
                 form_input_obj[split_current[0]] = split_current[1];
@@ -132,7 +148,7 @@
      * specific error messages. If no errors were found, the function returns true. Else, returns the
      * error_obj object.
      *
-     * @param   {Object}    form_input_obj  The object created by create_form_input_obj()
+     * @param   {Object}        form_input_obj  The object created by create_form_input_obj()
      * @returns {bool|Object}   True if no errors are found. Else returns error_obj object.
      */
     function create_error_obj(form_input_obj) {
