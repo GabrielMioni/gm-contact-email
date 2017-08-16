bded2473e4ea56f4eb25086a9a5ce2331b5260f9
GabrielMioni <gabriel.mioni@gmail.com>
2017-08-15 20:30:16 -0700

    Corrected package name.

--- gm-contact-email.php
+++ gm-contact-email.php
@@ -3,7 +3,7 @@
 require_once(ABSPATH . WPINC . '/class-phpmailer.php');
 
 /**
- * @package     GM-Contact-Email
+ * @package     GM-Contact-From
  * @author      Gabriel Mioni <gabriel@gabrielmioni.com>
  */
 
